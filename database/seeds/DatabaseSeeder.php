7f392de0c32bcb6ad6b6e0bb8724d0381ba4edda
Luna <ashe_the_frost_archer_8@hotmail.com>
2020-08-23 20:37:54 -0500

    nuevos cambios

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,10 +19,6 @@ class DatabaseSeeder extends Seeder
         factory(Price::class,500)->create();
         factory(Product::class,500)->create();
         factory(Client::class,100)->create();
-<<<<<<< HEAD
         factory(Income::class,100)->create();
-=======
-        factory(Income::class,10000)->create();
->>>>>>> 6edfb83834cd26c44f0d235ac5f869e6421ab2b0
     }
 }
